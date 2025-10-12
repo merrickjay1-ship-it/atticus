daf85b838f2d82a1255c4cd64edc5ac65bb88692
merrickjay1-ship-it <merrickjay1@gmail.com>
2025-10-12 13:50:20 -0600

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Atticus Family Office</h1>
-      <p>We’re live. More coming soon.</p>
-    </main>
-  );
-}
