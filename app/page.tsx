338ebc1740eb2ebbba04b7bafa1c6ca915fcc4f7
merrickjay1-ship-it <merrickjay1@gmail.com>
2025-10-12 13:35:24 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Atticus Family Office</h1>
+      <p>We’re live. More coming soon.</p>
+    </main>
+  );
+}
