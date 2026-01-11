fd2047febde3e7832848fa98b45d485ed8e5020d
merrickjay1-ship-it <merrickjay1@gmail.com>
2026-01-11 11:42:44 -0700

    Update route.ts

--- src/app/plaid/transactions/route.ts
+++ src/app/plaid/transactions/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/plaid/transactions/route.ts
-import { jsonErr, jsonOK, getItemRowForUser, plaidClient, userIdFrom } from '../../api/_lib/utils'
-';
+import { jsonErr, jsonOK, getItemRowForUser, plaidClient, userIdFrom } from "../../api/_lib/utils";
+
 
 export const runtime = 'nodejs';
 
